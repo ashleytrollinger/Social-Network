aca024d3b7191a88fd1272e1e5b86844c7b9afdd
ashleytrollinger <ashley@trollingers.com>
2023-07-10 20:13:36 -0400

    thoughtRoutes done

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,4 +1,15 @@
 const router = require('express').Router();
-const{
+const {
+    getAllThoughts,
+    getThoughtById,
+    addThought,
+    removeThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thoughtController');
 
-}
+router.route('./').get(getAllThoughts);
+router.route('/:userId').post(addThought);
+router.route('/:thoughtId').get(getThoughtById).put(addThought).delete(removeThought);
+router.route('/:thoughtId/reactions').post(addReaction).delete(removeThought);
+module.exports = router;
