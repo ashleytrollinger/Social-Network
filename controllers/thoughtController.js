70eb9f4ed57348cd8e0fcdf734cfd0f1e29e01b0
ashleytrollinger <ashley@trollingers.com>
2023-07-10 20:02:51 -0400

    thoughtControllers done

---
+++ controllers/thoughtController.js
@@ -0,0 +1,93 @@
+const { Thought, User, Types } = require('../models');
+
+const ThoughtController = {
+    getAllThoughts(req, res) {
+        Thought.find({})
+            .select("-__v")
+            .sort({ _id: -1 })
+            .then((dbThoughtData) => res.json(dbThoughtData))
+            .catch((err) => {
+                console.log(err)
+                res.status(400).json(err);
+            });
+    },
+    getThoughtById({ params }, res) {
+        console.log(params)
+        Thought.findOne({ _id: params.thougtId })
+            .select("-__v")
+            .then((dbThoughtData) => {
+                if (!dbThoughtData) {
+                    res.status(404).json()
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    addThought({ params, body }, res) {
+        console.log(body);
+        Thought.create(body)
+            .then(({ _id }) => {
+                return User.findOneAndUpdate(
+                    { _id: params.userId },
+                    { $push: { thoughts: _id } },
+                    { new: true }
+                );
+            })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+    removeThought({ params }, res) {
+        Thought.findOneAndDelete({ _id: params.thoughtId })
+            .then(deletedThought => {
+                if (!deletedThought) {
+                    return res.status(404).json
+                }
+                return User.findOneAndUpdate(
+                    { _id: params.username },
+                    { $pull: { thoughts: params.thoughtId } },
+                    { new: true }
+                );
+            })
+            .then(dbUserData => {
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+    addReaction({ params, body }, res) {
+        console.log(body)
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtid },
+            { $push: { reactions: body } },
+            { new: true }
+        )
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+    removeReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { reactions: { reactionId: params.reactionId } } }
+            { new: true }
+        )
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err));
+    }
+};
+
+module.exports = ThoughtController;
